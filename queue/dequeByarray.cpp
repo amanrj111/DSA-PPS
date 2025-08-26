09fba30ea8535ddeda59216bd27587f6824cd2e7
amanrj111 <156581242+amanrj111@users.noreply.github.com>
2025-08-26 19:31:21 +0530

    deque

--- queue/dequeByarray.cpp
+++ queue/dequeByarray.cpp
@@ -1,119 +1,118 @@
 #include <iostream>
-#include<queue>
-
 using namespace std;
 
-class Dequeue {
-  public:
+class Deque{
+public:
   int *arr;
   int size;
   int front;
   int rear;
 
-
-  Dequeue(int capacity){
-    arr = new int [capacity];
+  Deque(int capacity) {
+    arr  = new int[capacity];
     size = capacity;
     front = -1;
     rear = -1;
   }
 
-  void push(int value){
-    //overflow
-    //first element 
-    //normal inserttion 
-
-    if(rear == size-1){
-      cout << "overflow";
-      return;
+  void pushFront(int value) {
+    //3 cases: overflow, empty Queue me insert - single element, normal
+    if(front == 0) {
+      cout << "Oveflow " << endl;
     }
-    if(front == -1 && rear == -1){
-      front ++ ;
-      rear ++;
+    else if(front==-1 && rear==-1) {
+      front++;
+      rear++;
+      arr[front] = value;
+    }
+    else {
+      front--;
+      arr[front] = value;
+    }
+  }
+
+  void pushBack(int value) {
+    //3 cases: overflow, empty Queue me insert - single element, normal
+    if(rear == size - 1 ){
+      cout << "Oveflow " << endl;
+    }
+    else if(front==-1 && rear==-1) {
+      front++;
+      rear++;
       arr[rear] = value;
-    }else{
-      //normal insertion
+    }
+    else {
       rear++;
       arr[rear] = value;
     }
 
   }
 
-  void pop(){
-    //underflow
-    //single element
-    //normal pop
-
-    if(front == -1 && rear == -1){
-      cout << "underflow" << endl;
+  void popFront() {
+    //3 cases: underflow, single element, normal pop
+    if(front == -1 && rear == -1) {
+      cout << "Underflow" << endl;
     }
-    if(front == rear){
-      //single eelement 
+    else if(front == rear) {
       arr[front] = -1;
       front = -1;
-      rear = -1;
-    }else{
-      //normal pop
-       arr[front] = -1;
-        front++;
-      }
+      rear = -1; 
     }
-
-    int getSize(){
-    if(front == -1 && rear == -1){
-      return 0;
-    }
-    else{
-      return rear - front + 1;
+    else {
+      arr[front] = -1;
+      front++;
     }
   }
 
-  bool checkEmpty(){
-    if(front == -1 && rear == -1){
-      return true;
-    }else{
-      return false;
+  void popBack() {
+    //3 cases: underflow, single element, normal pop
+    if(front == -1 && rear == -1) {
+        cout << "Underflow" << endl;
     }
+    else if(front == rear) {
+        arr[rear] = -1;
+        front = -1;
+        rear = -1; 
+    }
+    else {
+      arr[rear] = -1;
+      rear--;
+    }
+
   }
 
- int  getFrontElement(){
-  if(front == -1){
-    cout << "there is no element" << endl;
-    return -1;
-  }
-  int value = arr[front];
-  return value;
-  }
-
-  int rearelement(){
-    if(rear == -1 ){
-      cout<< " there is no element " << endl;
-      return -1;
-    }else{
-      int value = arr[rear];
-      return value;
+  void print() {
+    for(int i=0; i<size; i++) {
+      cout << arr[i] << " ";
     }
+    cout << endl;
   }
 
 };
 
 
-int main(){
-   Dequeue q(5);
-   q.push(5);
-   q.push(15);
-   q.push(25);
 
-   cout << q.getSize() << endl;
+int main() {
+  Deque dq(7);
 
-   cout << q.getFrontElement() << endl;
+  dq.print();
+  dq.pushFront(10);
+  dq.pushBack(12);
+  dq.pushBack(14);
+  dq.pushBack(16);
+  dq.print();
 
-   q.pop();
-   q.pop();
-   q.pop();
+  dq.popFront();
+  dq.print();
 
-   cout << q.checkEmpty() << endl;
+  dq.popBack();
+  dq.print();
 
+  dq.pushFront(100);
+  dq.print();
+
+  dq.pushBack(200);
+  dq.print();
 
   return 0;
-} 
+}
---
+++ queue/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+void print(){
+//     if(front == -1 && rear == -1){
+//         cout << "Queue is empty" << endl;
+//         return;
+//     }
+//     for(int i = front; i <= rear; i++){
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
